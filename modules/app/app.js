acc9ee4b31fecad2e571eab5e502456a23974020
Jackie Abert <jax.abert@gmail.com>
2020-11-03 18:08:33 -0500

    cors fix

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,28 +1,33 @@
 'use strict';
 require('dotenv').config();
 const express = require('express')
-// const cors = require('cors')
-//const { CLIENT_ORIGIN } = require('../config');
+const cors = require('cors')
+const { CLIENT_ORIGIN } = require('../config');
 const { NODE_ENV } = require('../config');
 const app = express()
 
 app.use(helmet());
 
 // whitelist allowed origins
-// const allowedOrigins = ['http://localhost:3000', 'https://dsa-petful-client.jackie-abert.vercel.app'];
-// app.use(cors({
-//   origin: function(origin, callback){
-//     // allow requests with no origin - like mobile apps, curl, postman
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       const msg = 'The CORS policy for this site does not ' +
-//                   'allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
+const allowedOrigins = ['http://localhost:3000', 'https://dsa-petful-client.jackie-abert.vercel.app'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin - like mobile apps, curl, postman
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use(cors({
 //     origin: CLIENT_ORIGIN
 // }));
