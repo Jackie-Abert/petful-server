0ef3e3fbaef15e6fda28b11c709f15af8aa48c5c
Jackie Abert <jax.abert@gmail.com>
2020-11-03 16:00:17 -0500

    final commits

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const config = require('../config')
+const { CLIENT_ORIGIN } = require('../config');
+const { NODE_ENV } = require('../config');
 const app = express()
 
 app.use(cors({
@@ -9,5 +10,14 @@ app.use(cors({
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
-
+app.use(function errorHandler(error, req, res, next) {
+    let response;
+    if (NODE_ENV === 'production') {
+      response = { error: { message: 'server error' } };
+    } else {
+      console.error(error);
+      response = { message: error.message, error };
+    }
+    res.status(500).json(response);
+  });
 module.exports = app
